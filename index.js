c574c3e24090b43ab676ec3adc9bdd007d19b8df
Fredrik Olofsson <a@a.nu>
2013-09-05 23:51:32 +0200

    index.js - skeleton fixed

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
 ﻿module.exports = (function () {
-  
+
+  function init(options) {
+    //options.force to force a fast restart
+  }
+  function resize(size, options, cb) {
+    //options.force to force a fast resize
+  }
+  function restart(size, options, cb) {
+    //options.force to force a fast restart
+  }
+  function quit(size, options, cb) {
+    //options.force to force a fast quit
+  }
+
+  return {
+    init: init,
+    resize: resize,
+    restart: restart,
+    quit: quit
+  };
 
 })();
